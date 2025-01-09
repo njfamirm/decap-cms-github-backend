2ae80e3ee4e56963f6a3af5871bc6b8c0840ffac
Petr Czepiec <petr@czepiec.me>
2025-01-09 21:25:42 +0100

    feat: set scope with env

--- src/route/auth.ts
+++ src/route/auth.ts
@@ -26,7 +26,7 @@ nanoServer.route('GET', '/auth', (connection) => {
 
   const authorizationUri = client.authorizeURL({
     redirect_uri: `https://${host}/callback?provider=${provider}`,
-    scope: 'repo',
+    scope: config.scope,
     state: randomString(),
   });
 
